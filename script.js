433c7e50fd4ccd6dafe3c61fc2c7a4613da7075e
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2024-11-05 23:50:03 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const socialLinks = document.querySelector('.social-links');
-
-    menuToggle.addEventListener('click', function () {
-        socialLinks.style.display = socialLinks.style.display === 'flex' ? 'none' : 'flex';
-    });
-});
